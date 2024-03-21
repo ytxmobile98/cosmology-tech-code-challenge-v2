788f1c51a907d7666a97ad24ccc6e334ee2ed1fe
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2024-03-21 21:35:03 +0800

    test: buttons

--- code-challenge-v2/src/App.tsx
+++ code-challenge-v2/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled, { css } from 'styled-components'
+
+const Button = styled.button<{ $primary?: boolean; }>`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #BF4F74;
+  color: #BF4F74;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+
+  ${props => props.$primary && css`
+    background: #BF4F74;
+    color: white;
+  `}
+`;
+
+const Container = styled.div`
+  text-align: center;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container>
+      <Button>Normal Button</Button>
+      <Button $primary>Primary Button</Button>
+    </Container>
+  );
 }
 
-export default App
+export default App;
