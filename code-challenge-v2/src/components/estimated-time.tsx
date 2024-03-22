6cc5eaaaff99b42546cd56cdb6344ae90d4c5ed5
Tianxing Yang <5900105+ytxmobile98@users.noreply.github.com>
2024-03-22 09:24:58 +0800

    style: estimated time

--- code-challenge-v2/src/components/estimated-time.tsx
+++ code-challenge-v2/src/components/estimated-time.tsx
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+  margin: 1em 0;
+  padding: 0.5em;
+
+  border-radius: 0.25em;
+
   background-color: gainsboro;
   font-weight: 500;
 `;
